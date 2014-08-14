33979c7700f35344f3717b88e7851ce56ee88182
Louise Ireland <louireland@gmail.com>
2014-08-14 11:19:17 +0100

    Improvements to downloader

--- get-oceancolour.py
+++ get-oceancolour.py
@@ -6,6 +6,9 @@ import urllib
 import bz2
 import gdal
 import osr
+import numpy as np
+import datetime
+
 
 def getdata(filenames):
   '''
@@ -29,8 +32,8 @@ def getdata(filenames):
     print 'downloading...'
     urllib.urlretrieve(f_download, f_compress)
     print 'uncompressing file...'
+    print f_compress
     uncom = bz2.BZ2File(f_compress, 'r').read()
-    print ''
     output = open(f_uncompress, 'w')
     output.write(uncom)
     output.close()
@@ -38,6 +41,7 @@ def getdata(filenames):
     print 'creating geotif...'
     g = gdal.Open(f_uncompress)
     arr = g.ReadAsArray()
+    arr = np.array(arr)
     [cols, rows] = arr.shape
 
     outdata = gdal.GetDriverByName("GTiff")
@@ -46,6 +50,18 @@ def getdata(filenames):
     band.SetNoDataValue(nodata)
     dst_ds.SetGeoTransform(outgeo)
     dst_ds.SetProjection(outproj.ExportToWkt())
+    #set metadata tags
+    dst_ds.SetMetadataItem('SENSOR', 'AQUA_MODIS')
+    dst_ds.SetMetadataItem('RESOLUTION', '4km')
+    date = datetime.datetime.now()
+    date = date.strftime('%Y-%m-%d')
+    dst_ds.SetMetadataItem('DOWNLOAD_DATE', date)
+    dst_ds.SetMetadataItem('DOWNLOAD_FROM', 'NASA OCEANCOLOUR')
+    dst_ds.SetMetadataItem('PRODUCT_NAME', filename)
+    dst_ds.SetMetadataItem('UNITS', g.GetMetadataItem('Units'))
+    #maybe to make more general, import start time and end time from input data instead,
+    #so then would work with monthly and weekly...
+    dst_ds.SetMetadataItem('YEAR', g.GetMetadataItem('Start Year'))
     band.WriteArray(arr)
 
 
@@ -54,18 +70,29 @@ def getfilenames(minyear, maxyear):
   Input year 'from' and 'to' that you you want annual grids for.
   Returns a list of filenames.
   '''
+  validmin = 2002
+  validmax = 2013  
+
+  if minyear < validmin or maxyear > validmax:
+    raise UserWarning('Input years out of range. Choose between 2002 and 2013')
+ 
+  #leap years?
+  leapyears = [x for x in xrange(minyear, maxyear + 1)
+            if (x % 400 == 0) or (x % 4 == 0 and not x % 100 == 0)]
 
   filenames = []
   for year in range(minyear, maxyear+1):
-    print year
     #MODIS AQUA chl-a Annual 4km
-    filenames.append('A{0}001{0}365.L3m_YR_CHL_chlor_a_4km'.format(year))
+    if year in leapyears:
+      filenames.append('A{0}001{0}366.L3m_YR_CHL_chlor_a_4km'.format(year))
+    else:
+      filenames.append('A{0}001{0}365.L3m_YR_CHL_chlor_a_4km'.format(year))
 
   return filenames
   
 
 if __name__ == '__main__':
   
-  files = getfilenames(2002, 2004)
+  files = getfilenames(2000, 2004)
   getdata(files)
 
