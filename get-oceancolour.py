ad8fcc9fef54cb364a332488516db4d4b180e455
Louise Ireland <louireland@gmail.com>
2014-08-14 08:44:01 +0100

    Added date range functionality to downloader. Works with Annual data.

--- get-oceancolour.py
+++ get-oceancolour.py
@@ -1,5 +1,3 @@
-path_to_ftp = 'http://oceandata.sci.gsfc.nasa.gov/MODISA/Mapped/Annual/4km/chlor/'
-
 f = 'http://oceandata.sci.gsfc.nasa.gov/cgi/getfile/A20020012002365.L3m_YR_CHL_chlor_a_4km.bz2'
 
 workingdir = '/Users/Ireland/rsr/qgis-dev/'
@@ -9,7 +7,7 @@ import bz2
 import gdal
 import osr
 
-def getdata(filename):
+def getdata(filenames):
   '''
   Takes the filename which has been generated in 
   createfilename() and downloads and creates a GeoTiff
@@ -21,47 +19,53 @@ def getdata(filename):
   outproj = osr.SpatialReference()
   outproj.SetWellKnownGeogCS("WGS84")
   
-  outname = '{0}{1}.tif'.format(workingdir, filename)
+  for filename in filenames:
+    outname = '{0}{1}.tif'.format(workingdir, filename)
+
+    f_download   = 'http://oceandata.sci.gsfc.nasa.gov/cgi/getfile/{}.bz2'.format(filename)
+    f_compress   = '{0}{1}.bz2'.format(workingdir, filename)
+    f_uncompress = '{0}{1}'.format(workingdir, filename)
+
+    print 'downloading...'
+    urllib.urlretrieve(f_download, f_compress)
+    print 'uncompressing file...'
+    uncom = bz2.BZ2File(f_compress, 'r').read()
+    print ''
+    output = open(f_uncompress, 'w')
+    output.write(uncom)
+    output.close()
+
+    print 'creating geotif...'
+    g = gdal.Open(f_uncompress)
+    arr = g.ReadAsArray()
+    [cols, rows] = arr.shape
+
+    outdata = gdal.GetDriverByName("GTiff")
+    dst_ds = outdata.Create(outname, rows, cols, 1, gdal.GDT_Float32)
+    band = dst_ds.GetRasterBand(1)
+    band.SetNoDataValue(nodata)
+    dst_ds.SetGeoTransform(outgeo)
+    dst_ds.SetProjection(outproj.ExportToWkt())
+    band.WriteArray(arr)
 
 
-  f_download   = 'http://oceandata.sci.gsfc.nasa.gov/cgi/getfile/{}.bz2'.format(filename)
-  f_compress   = '{0}{1}.bz2'.format(workingdir, filename)
-  f_uncompress = '{0}{1}'.format(workingdir, filename)
+def getfilenames(minyear, maxyear):
+  '''
+  Input year 'from' and 'to' that you you want annual grids for.
+  Returns a list of filenames.
+  '''
 
-  print 'downloading...'
-  urllib.urlretrieve(f_download, f_compress)
-  print 'uncompressing file...'
-  uncom = bz2.BZ2File(f_compress, 'r').read()
-  print ''
-  output = open(f_uncompress, 'w')
-  output.write(uncom)
-  output.close()
+  filenames = []
+  for year in range(minyear, maxyear+1):
+    print year
+    #MODIS AQUA chl-a Annual 4km
+    filenames.append('A{0}001{0}365.L3m_YR_CHL_chlor_a_4km'.format(year))
+
+  return filenames
   
 
-  print 'creating geotif...'
-  g = gdal.Open(f_uncompress)
-  arr = g.ReadAsArray()
-  [cols, rows] = arr.shape
-
-  outdata = gdal.GetDriverByName("GTiff")
-  dst_ds = outdata.Create(outname, rows, cols, 1, gdal.GDT_Float32)
-  band = dst_ds.GetRasterBand(1)
-  band.SetNoDataValue(nodata)
-  dst_ds.SetGeoTransform(outgeo)
-  dst_ds.SetProjection(outproj.ExportToWkt())
-  band.WriteArray(arr)
-
-
-'''
-NEXT
-* Make into a function - this one downloads and converts a given file as input
-* Work out the geotransform
-* Save out as Geotif
-
-* Make a new function which selects the file to download based on date
-* Extend this to do a date range
-'''
-
-
 if __name__ == '__main__':
-  getdata('A20020012002365.L3m_YR_CHL_chlor_a_4km')
+  
+  files = getfilenames(2002, 2004)
+  getdata(files)
+
